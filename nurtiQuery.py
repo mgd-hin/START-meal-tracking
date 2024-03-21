883de8c86fc6b09c45056a3a609187baa2399d89
moritzslz <schultzmoritz@gmail.com>
2024-03-21 03:05:16 +0100

    much easier implementation of nutrition data fetching

--- nurtiQuery.py
+++ nurtiQuery.py
@@ -1,16 +1,49 @@
 import requests
+import openfoodfacts
+import json
 
+# User-Agent is mandatory
+api = openfoodfacts.API(user_agent="Grocerly/Prototype")
+
+def fetch_data_from_api(query):
+    data = api.product.text_search(query).get("products")
+    allergens = data[0].get("allergens")
+    nutrients = data[0].get("nutriments")
+    print(data[0]) #TODO Get the type of the food on order to recommend a better food of the same type with better nova score
+    #print(nutrients)
+    #print(allergens)
+    #print(nutrients["nova-group"])
+
+    data = {
+        "nutrients": {
+            "nova-group": nutrients["nova-group"],
+            "nutrition-score-fr_100g": nutrients["nutrition-score-fr_100g"],
+            "proteins_100g":  nutrients["proteins_100g"],
+            "salt_100g": nutrients["salt_100g"],
+            "sugars_100g": nutrients["sugars_100g"],
+            "saturated-fat_100g":  nutrients["saturated-fat_100g"],
+            "ph_100g": nutrients["ph_100g"],
+            "fat_100g": nutrients["fat_100g"],
+            "energy_100g": nutrients["energy_100g"],
+            "carbohydrates_100g": nutrients["carbohydrates_100g"]
+        },
+        "allergens": allergens
+    }
+
+    json_data = json.dumps(data)
+
+    print(json_data)
+
+fetch_data_from_api("water")
+
+
+
+
+# Deprecated:
+"""
 def fetch_data_from_api(query):
 
     search_url = "https://api.nal.usda.gov/fdc/v1/foods/search"
-
-    """
-    headers = {
-        "User-Agent": "My Custom User Agent",  # Example header
-        "Authorization": "Bearer qODbFNgUV6tkyuCkDhOxeL1aU74FjazhfbIOTCbh",  # Example authorization header
-        "Content-Type": "application/json"  # Example content type header
-    }
-    """
 
     try:
         response = requests.get(search_url + "?api_key=qODbFNgUV6tkyuCkDhOxeL1aU74FjazhfbIOTCbh&query=" + query)
@@ -19,6 +52,7 @@ def fetch_data_from_api(query):
         if response.status_code == 200:
             # Parse the JSON response
             data = response.json().get("foods")
+            print(data)
             nutrients = {}
             num_foods = len(data)
 
@@ -52,5 +86,6 @@ def fetch_data_from_api(query):
         return None
 
 # Example usage:
-data = fetch_data_from_api("Cheddar Cheese")
+#data = fetch_data_from_api("Cheddar Cheese")
 #print(requests.get("https://api.nal.usda.gov/fdc/v1/foods/search?api_key=qODbFNgUV6tkyuCkDhOxeL1aU74FjazhfbIOTCbh&query=tomato").json().get('foods')[0]["foodNutrients"])
+"""
