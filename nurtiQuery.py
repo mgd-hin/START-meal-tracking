49b13d5c21b445c36e11cdbeabc8b622bf645d86
moritzslz <schultzmoritz@gmail.com>
2024-03-21 18:40:50 +0100

    Deleted deprecated code

--- nurtiQuery.py
+++ nurtiQuery.py
@@ -107,56 +107,6 @@ def recommend_healthy_suggestions(json_data, nova_group, num_suggestions):
             }
             print(recommendation)
 
+
 # Example usage
 fetch_data_from_api("cookie")
-
-"""
-Deprecated:
-def fetch_data_from_api(query):
-
-    search_url = "https://api.nal.usda.gov/fdc/v1/foods/search"
-
-    try:
-        response = requests.get(search_url + "?api_key=qODbFNgUV6tkyuCkDhOxeL1aU74FjazhfbIOTCbh&query=" + query)
-
-        # Check if the request was successful (status code 200)
-        if response.status_code == 200:
-            # Parse the JSON response
-            data = response.json().get("foods")
-            print(data)
-            nutrients = {}
-            num_foods = len(data)
-
-            # Initialize the nutrients dictionary with zero values
-            for nutrient in data[0]["foodNutrients"]:
-                nutrients[nutrient.get("nutrientName")] = 0
-
-            # Sum up the nutrient values
-            for food in data:
-                for nutrient in food["foodNutrients"]:
-                    nutrient_name = nutrient.get("nutrientName")
-                    if nutrient_name in nutrients.keys():
-                        nutrients[nutrient_name] += nutrient.get("value")
-
-            # Calculate the average nutrient values
-            for nutrient_name, total_value in nutrients.items():
-                if nutrient_name in nutrients.keys():
-                    nutrients[nutrient_name] = total_value / num_foods
-
-            print(nutrients)
-
-            return data
-        else:
-            # Print an error message if the request was not successful
-            print(f"Error: Unable to fetch data from API (status code {response.status_code})")
-            return None
-
-    except requests.RequestException as e:
-        # Print an error message if there was a problem with the request
-        print(f"Error: {e}")
-        return None
-
-# Example usage:
-#data = fetch_data_from_api("Cheddar Cheese")
-#print(requests.get("https://api.nal.usda.gov/fdc/v1/foods/search?api_key=qODbFNgUV6tkyuCkDhOxeL1aU74FjazhfbIOTCbh&query=tomato").json().get('foods')[0]["foodNutrients"])
-"""
